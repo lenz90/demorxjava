3bb969e23d26ff5ae95f14fe83cf68a25d3954e8
Lizardo Mamani <lenz.90@gmail.com>
2018-07-22 19:56:22 -0500

    Creando la demo para RxJava2 Operadores

---
+++ src/main/java/pe/com/reactivex/lenz/demorxjava/DemorxjavaApplication.java
@@ -0,0 +1,12 @@
+package pe.com.reactivex.lenz.demorxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorxjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorxjavaApplication.class, args);
+	}
+}
